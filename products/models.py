b2f78476954f6f23d565171097a4993954297e97
HelloOjasMutreja <helloojasmutreja@gmail.com>
2025-09-05 00:38:21 +0530

    Setting up Product Model and using "api/" urls for Product app urls

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    image_url = models.URLField()
+    description = models.TextField(blank=True, null=True)
+    brand = models.CharField(max_length=100, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    source = models.CharField(max_length=50)  # e.g. Amazon, Myntra
+    product_url = models.URLField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.source}"
